a170835df8e29d3eaeb8e6507dcf3722872d4e78
HuyKhoiVN <huykhoi210vn@gmail.com>
2025-04-19 15:08:35 +0700

    dish order UI

--- CoffeeCRM.Data/ViewModels/InvoiceVM.cs
+++ CoffeeCRM.Data/ViewModels/InvoiceVM.cs
@@ -7,7 +7,7 @@
         public decimal TotalMoney { get; set; }
         public string PaymentStatus { get; set; }
         public DateTime CreatedTime { get; set; }
-        public int PaymentMethodId { get; set; }
+        public string PaymentMethod { get; set; }
         public int AccountId { get; set; }
         public string? TableName { get; set; }
         public string CashierName { get; set; }
